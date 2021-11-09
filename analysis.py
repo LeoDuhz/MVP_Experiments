477211cc6832e925b38f1bc07fc225fb07efd221
LeoDuhz <1119653713@qq.com>
2021-11-09 19:09:40 +0800

    add output analysis csv

--- analysis.py
+++ analysis.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import csv
 
-tips = pd.read_csv('./p22f.csv')
+type = "p22f"
+tips = pd.read_csv('./csv/'+type+'.csv')
 
 rad = tips["err_rad"].values.tolist()
 trans = tips["err_trans"].values.tolist()
@@ -12,7 +14,18 @@ length = len(rad)
 
 rate = [0,0.05,0.25,0.5,0.75,0.95,1]
 
+err_rad = []
+err_trans = []
+
 for i in range(len(rate)):
     index = min(max(int(rate[i] * length), 0), length-1)
     print("rate: ", rate[i], 'index', index, "err_rad: ", rad[index])
     print("rate: ", rate[i], 'index', index, "err_trans: ", trans[index])
+    err_rad.append(rad[index])
+    err_trans.append(trans[index])
+
+
+frame = pd.DataFrame([err_rad,err_trans], columns=['0','0.05','0.25','0.5','0.75','0.95','1'])
+# print(frame)
+frame.index = ["Rotation[rad]", "Translation[m]"]
+frame.to_csv("./output/"+type+".csv")
--- boxplot.py
+++ boxplot.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 
 
 tips = pd.read_csv('./csv/f2f.csv')
-tips.head()
 
 sns.boxplot(y=tips["err_rad"],data=tips)
 plt.show()
